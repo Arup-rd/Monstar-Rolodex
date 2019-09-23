36cdf0c5bd301cd093f98da835de49f5b964501b
Arup-rd <aruprd38@gmail.com>
2019-09-23 16:06:04 +0600

    Updates

---
+++ precache-manifest.378e26ee179cd7061300927097745553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213a985e71fd9dbb920023a503b9cec6",
+    "url": "/Monstar-Rolodex/index.html"
+  },
+  {
+    "revision": "7373cc86acbf52cf216e",
+    "url": "/Monstar-Rolodex/static/css/main.b2b999bc.chunk.css"
+  },
+  {
+    "revision": "c1b7fa00bb9a52a8493b",
+    "url": "/Monstar-Rolodex/static/js/2.0721cbce.chunk.js"
+  },
+  {
+    "revision": "7373cc86acbf52cf216e",
+    "url": "/Monstar-Rolodex/static/js/main.80a287ba.chunk.js"
+  },
+  {
+    "revision": "54020f0d86ea8548bd18",
+    "url": "/Monstar-Rolodex/static/js/runtime-main.d3b248e9.js"
+  }
+]);
